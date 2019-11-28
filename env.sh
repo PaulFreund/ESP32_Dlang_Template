ea88a6682db67b1f815d8cc6cce07516d109e761
Paul Freund <freund.paul@lvl3.org>
2019-11-28 13:35:16 +0100

    Fixed path not working reliably everywhere

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+# SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export PATH=$PATH:/opt/llvm-xtensa/bin:/opt/ldc-xtensa/bin
 export IDF_PATH=$SCRIPTPATH/esp-idf
